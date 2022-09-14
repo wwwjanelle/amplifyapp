2edfa1fb77aef3289e753ad69757d1638d32eb4d
Janelle Wallace <janelleawallace@gmail.com>
2022-09-14 09:53:48 -0400

    added api and storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "amplifyapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "s3f0739b18": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
